b4b728c421b181fc6f181d0e7e1e355d6c80e2b1
kur <rhdnfka94@gmail.com>
2020-03-05 11:55:52 +0900

    Rename: str -> name

--- img_app/src/App.js
+++ img_app/src/App.js
@@ -2,13 +2,11 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 
 function App() {
-    const [str, setStr] = useState("0");
+    const [name, setName] = useState("0");
 
     //const atest = async e => {
     const atest = e => {
         const f = e.target.files[0]
-        console.log(f)
-        console.log(f.text)
         
         //const text = await f.text() // It doesn't work in chrome version 72..
         // https://developer.mozilla.org/en-US/docs/Web/API/Blob/text
@@ -16,7 +14,7 @@ function App() {
         const reader = new FileReader();
         // https://developer.mozilla.org/ko/docs/Web/API/FileReader
         reader.addEventListener("load", e => {
-            setStr(e.target.result)
+            setName(f.name)
         })
         reader.readAsText(f)
     }
@@ -26,7 +24,7 @@ function App() {
           <div>
             <input type="file" id="input" onChange={atest}/>
           </div>
-          <h1>{str}</h1>
+          <h1>{name}</h1>
         </div>
     );
 }
